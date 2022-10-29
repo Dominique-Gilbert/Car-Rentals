85fa4f077d1857d194dcb737b24674c36e0dda78
Dominique <Dominique@DESKTOP-4TC02HD>
2022-10-29 15:27:54 +0200

    Add project files.

---
+++ Question 1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Question_1
+{
+    partial class frm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb5 = new System.Windows.Forms.RadioButton();
+            this.rb6 = new System.Windows.Forms.RadioButton();
+            this.cmb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(23, 35);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(51, 17);
+            this.rb1.TabIndex = 0;
+            this.rb1.Text = "1 day";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(23, 89);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(56, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.Text = "2 days";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(23, 148);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(56, 17);
+            this.rb3.TabIndex = 2;
+            this.rb3.Text = "3 days";
+            this.rb3.UseVisualStyleBackColor = true;
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(227, 35);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(56, 17);
+            this.rb4.TabIndex = 3;
+            this.rb4.Text = "4 days";
+            this.rb4.UseVisualStyleBackColor = true;
+            this.rb4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // rb5
+            // 
+            this.rb5.AutoSize = true;
+            this.rb5.Location = new System.Drawing.Point(227, 89);
+            this.rb5.Name = "rb5";
+            this.rb5.Size = new System.Drawing.Size(56, 17);
+            this.rb5.TabIndex = 4;
+            this.rb5.Text = "5 days";
+            this.rb5.UseVisualStyleBackColor = true;
+            // 
+            // rb6
+            // 
+            this.rb6.AutoSize = true;
+            this.rb6.Location = new System.Drawing.Point(227, 148);
+            this.rb6.Name = "rb6";
+            this.rb6.Size = new System.Drawing.Size(56, 17);
+            this.rb6.TabIndex = 5;
+            this.rb6.Text = "6 days";
+            this.rb6.UseVisualStyleBackColor = true;
+            // 
+            // cmb
+            // 
+            this.cmb.FormattingEnabled = true;
+            this.cmb.Items.AddRange(new object[] {
+            "COMPACT",
+            "STANDARD",
+            "LUXURY"});
+            this.cmb.Location = new System.Drawing.Point(256, 214);
+            this.cmb.Name = "cmb";
+            this.cmb.Size = new System.Drawing.Size(121, 21);
+            this.cmb.TabIndex = 6;
+            this.cmb.Text = "Selection";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Choose one of the following car styles";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(175, 305);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(61, 19);
+            this.lbl1.TabIndex = 8;
+            this.lbl1.Text = "TOTAL";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(358, 415);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(86, 34);
+            this.btn2.TabIndex = 10;
+            this.btn2.Text = "Exit";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Days You\'d Like To Rent The  Car \r\n";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.rb6);
+            this.groupBox1.Controls.Add(this.rb5);
+            this.groupBox1.Controls.Add(this.rb4);
+            this.groupBox1.Controls.Add(this.rb3);
+            this.groupBox1.Controls.Add(this.rb2);
+            this.groupBox1.Controls.Add(this.rb1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(297, 178);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // frm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(445, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmb);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "frm1";
+            this.Text = "CAR RENTALS";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.RadioButton rb5;
+        private System.Windows.Forms.RadioButton rb6;
+        private System.Windows.Forms.ComboBox cmb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Question 1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question_1
+{
+    public partial class frm1 : Form
+    {
+        //Declaring the Values of the car Styles.
+        const double num1 = 19.95;
+        const double num2 = 24.95;
+        const double num3 = 39.05;
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Checking if the combo box contains COMPACT.
+                if ((string)this.cmb.SelectedItem == "COMPACT")
+            {
+                //Checking which radio button the user selected.
+                if (rb1.Checked == true) 
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num1 * 1);
+                
+                }
+                if (rb2.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num1 * 2);
+
+                }
+                if (rb3.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num1 * 3);
+
+                }
+                if (rb4.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num1 * 4);
+
+                }
+                if (rb5.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num1 * 5);
+
+                }
+                if (rb6.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num1 * 6);
+
+                }
+
+            }
+            //Checking if the combo box contains STANDARD.
+            if ((string)this.cmb.SelectedItem == "STANDARD")
+            {
+                //Checking which radio button the user selected.
+                if (rb1.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num2 * 1);
+
+                }
+                if (rb2.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num2 * 2);
+
+                }
+                if (rb3.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num2 * 3);
+
+                }
+                if (rb4.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num2 * 4);
+
+                }
+                if (rb5.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num2 * 5);
+
+                }
+                if (rb6.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num2 * 6);
+
+                }
+            }
+            //Checking if the combo box contains LUXURY.
+            if ((string)this.cmb.SelectedItem == "LUXURY")
+            {
+                //Checking which radio button the user selected.
+                if (rb1.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num3 * 1);
+
+                }
+                if (rb2.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num3 * 2);
+
+                }
+                if (rb3.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num3 * 3);
+
+                }
+                if (rb4.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num3 * 4);
+
+                }
+                if (rb5.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num3 * 5);
+
+                }
+                if (rb6.Checked == true)
+                {
+                    lbl1.Text = "Total is $" + Convert.ToString(num3 * 6);
+
+                }
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            //This closes the application in the Exit button.
+            this.Close();
+        }
+    }
+}
